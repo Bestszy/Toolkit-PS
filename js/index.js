250d7563639a5ef5908f937ebc523faaa81adb70
Bart Koćmierowski <bartoszkocmierowski@gmail.com>
2019-10-03 22:16:24 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,153 @@
+// functions to enable and disable inputs*****TO DO-link repetition by getElementsByClassName
+document.getElementById("box").onchange=function(){
+  document.getElementById("form").disabled = !this.checked};
+document.getElementById("box2").onchange=function(){
+  document.getElementById("form2").disabled = !this.checked}
+
+  function mode (select){
+    var selection = select
+    var arryMode = document.getElementsByClassName("mode");
+    var arryRadio = document.getElementsByName('repetition');
+    //alert(eve);
+    if(selection==='wycena'){
+      Array.from(arryMode).forEach(function(item){item.setAttribute("disabled","disabled")});
+      Array.from(arryRadio).forEach(function(item){item.checked=false})
+    /*for(i=0; i<arr.length ; i++){
+      arr[i].setAttribute("disabled","disabled*/
+    
+    }else if(selection==="druk"){
+      Array.from(arryMode).forEach(function(item){item.removeAttribute("disabled")});
+    }
+    }
+
+//function to allow enter to forms only numbers***** TO DO-add exception ,, and change on . try use KeyboardEvent()
+function isNumberKey(evt){
+  var charCode = (evt.which) ? evt.which : evt.keyCode
+  if (charCode > 31 && (charCode != 46 &&(charCode < 48 || charCode > 57)))
+      return false;
+  return true;
+}
+//function triggered by button apply to change dimensions
+function resizeAply(){
+  var  IWidth = document.getElementById("inputWidth").value;
+  var  IHeight = document.getElementById("inputHeight").value;
+  var  IDpi = document.getElementById("inputDPI").value;
+  var sizeButtonSender = 'resizAply ={reWidth:"'+IWidth+'",reHeight:"'+IHeight+'",reDpi:"'+IDpi+'"};'
+  alert(sizeButtonSender );
+  csInterface.evalScript(sizeButtonSender + "resizeButton()",);
+}
+
+  function widthF(){
+    let inputWidth = document.getElementById("inputWidth").value;
+    document.getElementById("inputDPI").value = (parseFloat(pixelW)/(inputWidth/2.54)).toFixed(3);
+    document.getElementById("inputHeight").value =(parseFloat(pixelH)/((parseFloat(pixelW)/(inputWidth/2.54))/2.54)).toFixed(2);
+  }
+  function heightF(){
+    let inputHeight = document.getElementById("inputHeight").value;
+    document.getElementById("inputDPI").value = (parseFloat(pixelH)/(inputHeight/2.54)).toFixed(3);
+    document.getElementById("inputWidth").value = (parseFloat(pixelW)/((parseFloat(pixelH)/(inputHeight/2.54))/2.54)).toFixed(2);
+  }
+  function dpiF(){
+    let inputDPI = document.getElementById("inputDPI").value;
+    document.getElementById("inputHeight").value = (parseFloat(pixelH)/(inputDPI/2.54)).toFixed(2);
+    document.getElementById("inputWidth").value = (parseFloat(pixelW)/(inputDPI/2.54)).toFixed(2);
+  }
+var csInterface = new CSInterface();
+//repe = document.getElementsByName("repetition");
+//var test = 'test asf';
+//document.getElementById("test").innerHTML=test;
+/* Make a reference to your HTML button and add a clilck handler. */
+function directory(x){
+  var newPath = "";
+  var newName = "";
+  var fabric = x;
+
+  var closeFile = document.getElementById("closeFile").checked?true:false;
+  var printingItem = document.getElementById("druk").checked?true:false;
+  alert(printingItem)
+  //repe = document.getElementsByName("repetition");
+ //file
+  if(document.getElementById("box").checked && document.getElementById("form").value.length <= 0){
+    return alert('enter the name')
+  }else{document.getElementById("box").checked && document.getElementById("form").value.length > 0? 
+  newName = document.getElementById("form").value : newName = ""};
+//subfolder
+  if(document.getElementById("box2").checked && document.getElementById("form2").value.length <= 0){
+    return alert('enter the subfolder')
+  }else{document.getElementById("box2").checked && document.getElementById("form2").value.length > 0? 
+  newPath = document.getElementById("form2").value : newPath = ""};
+
+  var  IWidth = document.getElementById("inputWidth").value;
+  var  IHeight = document.getElementById("inputHeight").value;
+  var  IDpi = document.getElementById("inputDPI").value;
+if(printingItem===true){
+  repe = document.getElementsByName("repetition");
+  for(i=0; i<repe.length; i++){
+    if(repe[i].checked){
+    repeti = repe[i].value;
+    //alert(repeti)
+    }
+  }}else{ repeti = "";
+  alert("set repetition")
+}
+  //alert(repeti)
+
+//document.getElementById("toCopy").innerHTML=path + nameee;
+
+  /*}else if(document.getElementById("box2").checked && document.getElementById("form2").value.length <= 0){
+    return alert('"enter subfolder name"')
+  }else if(document.getElementById("box2").checked && document.getElementById("form2").value.length > 0){ 
+    newPath = document.getElementById("form2").value;
+    alert(newPath)
+  };*/
+  var sender = ('var fabric={fabric:"'+fabric+'",newName:"'+newName+'",newPath:"'+newPath+'",closeFile:"'+closeFile+'",printingItem:"'+printingItem+'",reDpi:"'+IDpi+'",repetition:"'+repeti+'"};');
+  //var sender =('var fabric ="' + fabric + '";');
+  //alert(sender)
+  csInterface.evalScript(sender + "saveDocument()",);
+}
+//var nameToPass=""
+function inner(){
+  csInterface.evalScript('getDocData()', function(res){
+    var infoArr = res.split('§');
+    document.getElementById("width").innerHTML=infoArr[0];
+    document.getElementById("inputWidth").value=(parseFloat(infoArr[8])).toFixed(2);
+    document.getElementById('height').innerHTML=infoArr[1];
+    document.getElementById("inputHeight").value=(parseFloat(infoArr[9])).toFixed(2);
+    document.getElementById('reso').innerHTML=infoArr[2];
+    document.getElementById('inputDPI').value=infoArr[2];
+    document.getElementById('colorType').innerHTML=infoArr[3];
+    document.getElementById('printingProfile').innerHTML=infoArr[4];
+    document.getElementById('pathnon').innerHTML=infoArr[5];
+    document.getElementById("toCopy").innerHTML=infoArr[7]
+
+    return cmW = infoArr[8],
+    pixelW = infoArr[0],
+    pixelH = infoArr[1]
+
+    
+
+  })
+};
+
+csInterface.addEventListener('documentEdited',inner);
+csInterface.addEventListener('documentAfterActivate',inner);
+
+function copyElementText(id) {
+  var text = document.getElementById(id).innerText;
+  var elem = document.createElement("textarea");
+  document.body.appendChild(elem);
+  elem.value = text;
+  elem.select();
+  document.execCommand("copy");
+  document.body.removeChild(elem);
+}
+
+//setTimeout(inner(),1000);
+/*var openKreton = document.getElementById("kreton");
+openKreton.addEventListener("click", openDoc, false);
+var openSatyna = document.getElementById("satyna");
+openSatyna.addEventListener("click", openDoc, false);
+/* Write a helper function to pass instructions to the ExtendScript side. */
+/*function openDoc() {
+  csInterface.evalScript("saveDocument()");
+}*/
---
+++ js/index.jsx
@@ -0,0 +1,160 @@
+/*var json2 = File($.fileName).path + "/" + "json2.js";
+$.evalFile(json2); I can't include json to jsx */
+function currentDate(){
+    var date = new Date()
+    var year = date.getFullYear().toString();
+    var month = (date.getMonth() +1).toString();
+    var day = date.getDate().toString();
+    if (month.length == 1){ month = '0' + month}
+    if (day.length == 1){ day = '0' + day}
+    return year + month + day
+};
+function resizeButton(){
+  preferences.rulerUnits = Units.CM;
+  var doc = app.activeDocument;
+  doc.resizeImage(undefined, undefined, resizAply.reDpi, ResampleMethod.NONE)
+}
+
+  function saveDocument(){
+//var docCopy = app.activeDocument.duplicate();
+//var doc = docCopy;
+var orginalRulerUnits = preferences.rulerUnits; // has to be in the function
+//preferences.rulerUnits = Units.PIXELS;
+preferences.rulerUnits = Units.CM;
+var doc = app.activeDocument;
+if(fabric.newName.length>0){
+var name = fabric.newName;
+}else{
+var name = doc.name
+};
+var r = fabric.repetition
+//var name = doc.name
+/*if(fabric.newName.length > 0){
+  return name = fabric.newName}*/
+//var check = 'WYCENA-'+ currentDate();
+
+doc.flatten(); //flats whole file 
+doc.changeMode(ChangeMode.CMYK); //converts to cmyk 
+doc.colorProfileName='U.S. Web Coated (SWOP) v2';
+/*var nameChecked = 'assd';
+if(name.slice(0,15) === check){nameChecked = name.slice(15)
+}else{nameChecked = name}*/
+if(fabric.printingItem === "true"){
+doc.resizeImage(undefined, undefined, fabric.reDpi, ResampleMethod.NONE)
+var res = doc.resolution.toFixed(0)
+var fileNameItem = 'DRUK-' + currentDate() + '-' + name +'-'+res+'-'+r ;
+if (fabric.fabric == 'kreton') {var path = "Y:\\_pliki-zamowienia\\!KRETON"; 
+} else if(fabric.fabric == 'satyna') {var path = "Y:\\_pliki-zamowienia\\!SATYNA";
+} else if(fabric.fabric == 'woal') {var path = "Y:\\_pliki-zamowienia\\!WOAL";
+} else if(fabric.fabric == 'płótno') {var path = "Y:\\_pliki-zamowienia\\!PŁÓTNO-LEN";
+} else if(fabric.fabric == 'sofia') {var path = "Y:\\_pliki-zamowienia\\!SOFIA";
+} else if(fabric.fabric == 'panama') {var path = "Y:\\_pliki-zamowienia\\!PANAMA";
+} else if(fabric.fabric == 'gabardyna') {var path = "Y:\\_pliki-zamowienia\\!GABARDYNA";
+} else if(fabric.fabric == 'jersey 180') {var path = "Y:\\_pliki-zamowienia\\!JERSEY 180";
+} else if(fabric.fabric == 'jersey 200') {var path = "Y:\\_pliki-zamowienia\\!JERSEY 200";
+} else if(fabric.fabric == 'jersey 230') {var path = "Y:\\_pliki-zamowienia\\!JERSEY 230";
+} else if(fabric.fabric == 'interlock') {var path = "Y:\\_pliki-zamowienia\\!INTERLOCK";
+} else if(fabric.fabric == 'pętelka 250') {var path = "Y:\\_pliki-zamowienia\\!PĘTELKA 250";
+} else if(fabric.fabric == 'pętelka 330') {var path = "Y:\\_pliki-zamowienia\\!PĘTELKA 330";
+}
+alert("true" + path + fileNameItem);
+var file = new File(path + '/' + fileNameItem);
+                opts = new TiffSaveOptions();
+                opts.imageCompression = TIFFEncoding.TIFFLZW; 
+                doc.saveAs(file, opts, true);
+} else{
+var fileName = 'WYCENA-' + currentDate() + '-' + name;
+
+if (fabric.fabric == 'kreton') {var path = "Y:\\_pliki-zamowienia\\!KRETON\\wycena"; 
+} else if(fabric.fabric == 'satyna') {var path = "Y:\\_pliki-zamowienia\\!SATYNA\\wycena";
+} else if(fabric.fabric == 'woal') {var path = "Y:\\_pliki-zamowienia\\!WOAL\\wycena";
+} else if(fabric.fabric == 'płótno') {var path = "Y:\\_pliki-zamowienia\\!PŁÓTNO-LEN\\wycena";
+} else if(fabric.fabric == 'sofia') {var path = "Y:\\_pliki-zamowienia\\!SOFIA\\wycena";
+} else if(fabric.fabric == 'panama') {var path = "Y:\\_pliki-zamowienia\\!PANAMA\\wycena";
+} else if(fabric.fabric == 'gabardyna') {var path = "Y:\\_pliki-zamowienia\\!GABARDYNA\\wycena";
+} else if(fabric.fabric == 'jersey 180') {var path = "Y:\\_pliki-zamowienia\\!JERSEY 180\\wycena";
+} else if(fabric.fabric == 'jersey 200') {var path = "Y:\\_pliki-zamowienia\\!JERSEY 200\\wycena";
+} else if(fabric.fabric == 'jersey 230') {var path = "Y:\\_pliki-zamowienia\\!JERSEY 230\\wycena";
+} else if(fabric.fabric == 'interlock') {var path = "Y:\\_pliki-zamowienia\\!INTERLOCK\\wycena";
+} else if(fabric.fabric == 'pętelka 250') {var path = "Y:\\_pliki-zamowienia\\!PĘTELKA 250\\wycena";
+} else if(fabric.fabric == 'pętelka 330') {var path = "Y:\\_pliki-zamowienia\\!PĘTELKA 330\\wycena";
+}
+var f = new Folder( path + '/'+currentDate()+ '/' + fabric.newPath );
+if (!f.exists) { f.create()};
+//if(name.slice(0,15)=== check)
+var file = new File(f + '/' + fileName);
+                opts = new TiffSaveOptions();
+                opts.imageCompression = TIFFEncoding.TIFFLZW; 
+                doc.saveAs(file, opts, true);}
+//if(!file.exists){file.create()};
+//references.rulerUnits = Units.PIXELS
+alert('The file has been saved.' + 
+'\n name: '+ fileName + 
+'\n dpi: '+doc.resolution.toFixed(0) + 
+'\n size: '+ Number(doc.width).toFixed(2)+' cm' +' x '+ Number(doc.height).toFixed(2)+' cm'+
+'\n path: '+app.activeDocument.path);
+//var doc = docCopy;
+
+if(fabric.closeFile === "true"){doc.close(SaveOptions.DONOTSAVECHANGES)};
+  }
+  function getDocData(){
+    preferences.rulerUnits = Units.PIXELS;
+    var width = app.activeDocument.width;
+    var height = app.activeDocument.height;
+    preferences.rulerUnits = Units.CM;
+    var widthCM = app.activeDocument.width;
+    var heightCM = app.activeDocument.height;
+    var name = app.activeDocument.name;
+    try{
+      if (fabric.fabric == 'kreton') {var mat= "Y:\\_pliki-zamowienia\\!KRETON\\wycena"; 
+} else if(fabric.fabric == 'satyna') {var mat= "Y:\\_pliki-zamowienia\\!SATYNA\\wycena";
+} else if(fabric.fabric == 'woal') {var mat= "Y:\\_pliki-zamowienia\\!WOAL\\wycena";
+} else if(fabric.fabric == 'płótno') {var mat= "Y:\\_pliki-zamowienia\\!PŁÓTNO-LEN\\wycena";
+} else if(fabric.fabric == 'sofia') {var mat= "Y:\\_pliki-zamowienia\\!SOFIA\\wycena";
+} else if(fabric.fabric == 'panama') {var mat= "Y:\\_pliki-zamowienia\\!PANAMA\\wycena";
+} else if(fabric.fabric == 'gabardyna') {var mat= "Y:\\_pliki-zamowienia\\!GABARDYNA\\wycena";
+} else if(fabric.fabric == 'jersey 180') {var mat= "Y:\\_pliki-zamowienia\\!JERSEY 180\\wycena";
+} else if(fabric.fabric == 'jersey 200') {var mat= "Y:\\_pliki-zamowienia\\!JERSEY 200\\wycena";
+} else if(fabric.fabric == 'jersey 230') {var mat= "Y:\\_pliki-zamowienia\\!JERSEY 230\\wycena";
+} else if(fabric.fabric == 'interlock') {var mat= "Y:\\_pliki-zamowienia\\!INTERLOCK\\wycena";
+} else if(fabric.fabric == 'pętelka 250') {var mat= "Y:\\_pliki-zamowienia\\!PĘTELKA 250\\wycena";
+} else if(fabric.fabric == 'pętelka 330') {var mat= "Y:\\_pliki-zamowienia\\!PĘTELKA 330\\wycena";
+};
+  } catch(e) {
+    var mat = 'first save the world';
+  };
+    try{  
+      var printingProfile = app.activeDocument.colorProfileName;  
+  } catch(e) {  
+      var printingProfile = "not embedded";  
+  };
+    var colorType = app.activeDocument.mode;
+    var reso= app.activeDocument.resolution;
+    var sender= (''+width+'§'+height+'§'+reso+'§'+printingProfile+'§'+colorType+'§'+name+'§'+currentDate()+'§'+mat+'§'+widthCM+'§'+heightCM+'');
+    return sender
+    };
+  //alert('The file has been saved.' + 
+//'\n name: '+ fileName + 
+//'\n dpi: '+doc.resolution.toFixed(0) + 
+//'\n size: '+ Number(doc.width).toFixed(2)+' cm' +' x '+ Number(doc.height).toFixed(2)+' cm'+
+//'\n path: '+app.activeDocument.path);
+
+  
+            /*function saveTiff(name) {
+                if (insertData.typeOfFabric === 'satyna'){var myFolder = "C:\\Users\\AdminX\\Desktop\\Bart\\smart Saver\\DEMO\\satyna"} 
+                else if(insertData.typeOfFabric === 'kreton'){var myFolder = "C:\\Users\\AdminX\\Desktop\\Bart\\smart Saver\\DEMO\\kreton"}
+                var file = new File(myFolder + '/' + name + '.tiff');
+                var opts = new TiffSaveOptions();
+                opts.imageCompression.TIFFLZW;
+                doc.saveAs(file, opts, false);
+            }saveTiff(fileName)
+        }
+};
+preferences.rulerUnits = Units.CM
+alert('The file has been saved.\n name: '+insertData.name+
+'\n dpi: '+parseFloat((doc.resolution).toFixed(3))+
+'\n size: '+ Number(doc.width).toFixed(2)+' cm' +' x '+ Number(doc.height).toFixed(2)+' cm'+
+'\n orderID: '+insertData.order+
+'\n repetition: '+insertData.repetition[0]+
+'\n path: '+app.activeDocument.path
+);*/
